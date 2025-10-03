c7f506ab4b461d5eab2fe789ab35b2950fa95140
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-10-03 11:14:24 -0700

    Completed Chapter 02 Ex 09-14

---
+++ Chapter02/Exercises/Chapter02Exercise13.java
@@ -0,0 +1,36 @@
+/*
+Modify the code from Exercise 12 to produce:
+99999888887777766666555554444433333222221111100000
+99999888887777766666555554444433333222221111100000
+99999888887777766666555554444433333222221111100000
+99999888887777766666555554444433333222221111100000
+99999888887777766666555554444433333222221111100000
+*/
+public class Chapter02Exercise13 {
+
+  public static final int LINES = 5;
+  public static final int REPEAT = 5;
+  public static final int NUMBERS = 9;
+
+  public static void main(String[] args) {
+
+    repeat();
+
+  }
+
+  // method for numbers
+  public static void numbers() {
+    for (int i = NUMBERS; i >= 0; i --) {
+      for (int j = 1; j <= REPEAT; j++) {
+          System.out.print(i);
+      }
+    }
+  }
+
+  public static void repeat() {
+    for (int i = 1; i <= LINES; i++) {
+      numbers();
+      System.out.println();
+    }
+  }
+}
