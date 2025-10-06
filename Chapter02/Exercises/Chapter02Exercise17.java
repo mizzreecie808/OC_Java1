521ba8cbf987f875b3d7dd60489fc6213f23cafb
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-10-05 19:57:12 -0700

    Completed Ch 02 Exercise 16 - 21

---
+++ Chapter02/Exercises/Chapter02Exercise17.java
@@ -0,0 +1,48 @@
+/*
+Modify your SlashFigure program from the previous exercise to become a
+new program called SlashFigure2 that uses a global constant for the
+figure’s height. (You may want to make loop tables first.)
+The previous output used a constant height of 6.
+The following are the outputs for constant heights of 4 and 8:
+!!!!!!!!!!!!!!
+\\!!!!!!!!!!//
+\\\\!!!!!!////
+\\\\\\!!//////
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+\\!!!!!!!!!!!!!!!!!!!!!!!!!!//
+\\\\!!!!!!!!!!!!!!!!!!!!!!////
+\\\\\\!!!!!!!!!!!!!!!!!!//////
+\\\\\\\\!!!!!!!!!!!!!!////////
+\\\\\\\\\\!!!!!!!!!!//////////
+\\\\\\\\\\\\!!!!!!////////////
+\\\\\\\\\\\\\\!!//////////////
+*/
+public class Chapter02Exercise17 {
+  public static final int SIZE = 4;
+
+  public static void main(String[] args) {
+
+    for (int line = 1; line <= SIZE; line++) {
+      int slash = 2 * line - 2;
+      int exclamation = -4 * line + 4 * SIZE + 2;
+
+      // 1. inner loop backslahes (\\)
+      for (int i = 1; i <= slash; i++) {
+        System.out.print("\\");
+      }
+
+      // 2. inner loop exclamation (!)
+      for (int i = 1; i <= exclamation; i++) {
+        System.out.print("!");
+      }
+
+      // 3. inner loop forward slash (/)
+      for (int i = 1; i <= slash; i++) {
+        System.out.print("/");
+      }
+      System.out.println();
+    }
+
+  }
+}
